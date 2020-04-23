34a6e803303ead91b7accdcfabfab14ea53c7f57
cebasypia <cebasypia.tskm8@gmail.com>
2020-04-23 14:52:57 +0900

    [Finish] 5-5 Add MyPage

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'post_images#index'
   devise_for :users
   resources :post_images, only: %i[new create index show destroy] do
     resource :favorites, only: %i[create destroy]
     resource :post_comments, only: %i[create]
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'post_images#index'
+  resources :users, only: [:show]
 end
